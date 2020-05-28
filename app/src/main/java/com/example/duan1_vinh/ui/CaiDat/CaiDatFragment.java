6f96b9563f3da4ceb8c8d29a390b5acef37740d6
mahuha80 <dracula01112000@gmail.com>
2020-05-28 22:43:51 +0700

    fix ui

--- app/src/main/java/com/example/duan1_vinh/ui/CaiDat/CaiDatFragment.java
+++ app/src/main/java/com/example/duan1_vinh/ui/CaiDat/CaiDatFragment.java
@@ -1,16 +1,27 @@
 package com.example.duan1_vinh.ui.CaiDat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.Person;
+import androidx.core.graphics.drawable.IconCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -18,17 +29,31 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.duan1_vinh.R;
 import com.example.duan1_vinh.dao.KhoanThuDAO;
 import com.example.duan1_vinh.model.KhoanThu;
+import com.example.duan1_vinh.ui.MainActivity;
 import com.example.duan1_vinh.ui.ThongKeChiTiet.ThongKeChiTietModel;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CaiDatFragment extends Fragment {
 
 
-
     private ThongKeChiTietModel thongKeChiTietModel;
+    Button btnRating;
+    private Context context;
+    NotificationManager notificationManager;
+    NotificationCompat.Builder builder;
+    NotificationChannel channel;
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,6 +61,48 @@ public class CaiDatFragment extends Fragment {
                 ViewModelProviders.of(this).get(com.example.duan1_vinh.ui.ThongKeChiTiet.ThongKeChiTietModel.class);
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnRating = view.findViewById(R.id.btnRating);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        CharSequence name = "My Notification";
+        String description = "yadda yadda";
+        channel = new NotificationChannel("1", name, importance);
+        channel.setDescription(description);
+
+        builder = new NotificationCompat.Builder(context.getApplicationContext(), channel.getId())
+                .setSmallIcon(R.drawable.group);
+
+        notificationManager.createNotificationChannel(channel);
+
+        btnRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Person jd = new Person.Builder()
+                        .setName("QUAN LI THU CHI")
+                        .setImportant(true)
+                        .build();
+
+                new NotificationCompat.MessagingStyle(jd)
+                        .addMessage("thank you for rating us", new Date().getTime(), jd)
+                        .setBuilder(builder);
+
+
+                notificationManager.notify(1, builder.build());
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/duan1_vinh/ui/KhoanChi_TKCT.java
+++ app/src/main/java/com/example/duan1_vinh/ui/KhoanChi_TKCT.java
@@ -11,26 +11,28 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.duan1_vinh.R;
-import com.example.duan1_vinh.adapter.KhoanThuTKCTAdapter;
-import com.example.duan1_vinh.dao.KhoanThuDAO;
-import com.example.duan1_vinh.model.KhoanThu;
+import com.example.duan1_vinh.adapter.KhoanChiTKCTAdapter;
+import com.example.duan1_vinh.dao.KhoanChiDAO;
+import com.example.duan1_vinh.model.KhoanChi;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class KhoanThu_TKCT extends Fragment {
+public class KhoanChi_TKCT extends Fragment {
     private ListView listView;
     private Context context;
-    private KhoanThuTKCTAdapter khoanThuTKCTAdapter;
-    private KhoanThuDAO khoanThuDAO;
+    private KhoanChiTKCTAdapter khoanChiTKCTAdapter;
+    private KhoanChiDAO khoanChiDAO;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        khoanThuDAO.closeDB();
+        khoanChiDAO.closeDB();
     }
 
     @Override
@@ -42,30 +44,41 @@ public class KhoanThu_TKCT extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root=LayoutInflater.from(context).inflate(R.layout.fragment_khoanthu_tkct,container,false);
+        View root = LayoutInflater.from(context).inflate(R.layout.fragment_khoanchi_tkct, container, false);
         return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        listView = view.findViewById(R.id.lv_thongkechitiet_khoanthu);
+        listView = view.findViewById(R.id.lv_thongkechitiet_khoanchi);
+        swipeRefreshLayout=view.findViewById(R.id.pullToRefresh1);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        khoanThuDAO = new KhoanThuDAO(context);
-        List<KhoanThu> list = new ArrayList<>();
+        khoanChiDAO = new KhoanChiDAO(context);
+        List<KhoanChi> list = new ArrayList<>();
         try {
-            list = khoanThuDAO.listKhoanThu();
+            list = khoanChiDAO.listKhoanChi();
         } catch (ParseException e) {
             e.printStackTrace();
             Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show();
         }
-        khoanThuTKCTAdapter = new KhoanThuTKCTAdapter(context, list);
-        listView.setAdapter(khoanThuTKCTAdapter);
-
+        try {
+            khoanChiTKCTAdapter = new KhoanChiTKCTAdapter(context, list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Loi", Toast.LENGTH_SHORT).show();
+        }
+        listView.setAdapter(khoanChiTKCTAdapter);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
 
     }
 }
